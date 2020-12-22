55e45363d3015adfd958b9f67cd714241aad7d87
Stephen <smith_sra@yahoo.co.uk>
2020-12-22 21:41:00 +0000

    Deploy

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://limitless-refuge-19848.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -95,7 +95,7 @@ class App extends React.Component {
     //       this.state.input)
     .then(response => {
       if(response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://limitless-refuge-19848.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
